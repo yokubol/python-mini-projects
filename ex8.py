0b6cb7a011a9ed1a4daffcbe67d10fb4a995b8ea
Suppawat Yokubol <s.yokubol@gmail.com>
2018-07-11 20:14:29 +0200

    add ex8.py

---
+++ ex8.py
@@ -0,0 +1,17 @@
+# assign a variable as a formatted string with 4 substitutable brackets
+formatter = "{} {} {} {}"
+# display numbers on the screen using formatted string
+print(formatter.format(1, 2, 3, 4))
+# display words on the screen using formatted string
+print(formatter.format("one", "two", "three", "four"))
+# display boolean values on the screen using formatted string
+print(formatter.format(True, False, False, True))
+# insert formatter into formatted strings and display on the screen
+print(formatter.format(formatter, formatter, formatter, formatter))
+# insert phrases into formatters and display them on the screen
+print(formatter.format(
+	"Try your",
+	"Own text here",
+	"Maybe a poem",
+	"Or a song about fear"
+))
