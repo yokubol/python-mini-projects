380827dcdee6e3c6899dd657caf5a1d732b90daf
Suppawat Yokubol <s.yokubol@gmail.com>
2018-07-12 22:55:37 +0200

    add

--- ex11e1.py
+++ ex11e1.py
@@ -1,5 +1,7 @@
+# prompt user for information and assign values as variables
 age = input("How old are you? ")
 height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
+# display those information in a complete sentence
 print(f"So you're {age} old, {height} tall and {weight} heavy.")
--- ex13.py
+++ ex13.py
@@ -1,7 +1,10 @@
+# import sys module
 from sys import argv
-# read the WYSS section for how to run this
+
+# unpack the argument variable
 script, first, second, third = argv
 
+# display all retrieved information on the screen
 print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
