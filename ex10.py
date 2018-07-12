4554f980552ce10389ef16902408c8e670475d86
Suppawat Yokubol <s.yokubol@gmail.com>
2018-07-12 16:27:27 +0200

    add

---
+++ ex10.py
@@ -0,0 +1,16 @@
+# assign variables as strings with escape symbols
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+# assign a variable with escape symbols using three double-quotes
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+# display strings from assigned variables above
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
